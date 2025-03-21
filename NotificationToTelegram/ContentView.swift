f47be0cfcc69685536d2ea8ee824b24f656ba692
Muhammed Elşami <muhammed.elsami@detaysoft.com>
2025-03-21 13:37:16 +0300

    Initial Commit

---
+++ NotificationToTelegram/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NotificationToTelegram
+//
+//  Created by Muhammed Elşami on 21.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NotificationToTelegram/NotificationToTelegramApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationToTelegramApp.swift
+//  NotificationToTelegram
+//
+//  Created by Muhammed Elşami on 21.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct NotificationToTelegramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
